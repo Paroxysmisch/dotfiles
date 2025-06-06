f5e91735209da399069af9b0233a7cdbb1b9c626
Yash Shah <yashshah00@outlook.com>
2025-06-06 22:01:39 +0100

    Added separate Darwin and Linux wezterm configurations.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,49 +5,62 @@ local config = wezterm.config_builder()
 config.color_scheme = "Blue Scheme"
 
 config.color_schemes = {
-  ["Blue Scheme"] = {
-    background = "#0d1117",
-  },
+	["Blue Scheme"] = {
+		background = "#0d1117",
+	},
 }
 
 config.initial_rows = 200
 config.initial_cols = 300
 
+local is_darwin <const> = wezterm.target_triple:find("darwin") ~= nil
+local is_linux <const> = wezterm.target_triple:find("linux") ~= nil
 local font_size = 16
+if is_darwin then
+	font_size = 16
+elseif is_linux then
+	font_size = 12
+end
 
-config.window_frame = {
-  font_size = font_size,
-  inactive_titlebar_bg = 'rgba(5% 6% 9% 80%)',
-}
+if is_darwin then
+	config.window_frame = {
+		font_size = font_size,
+		inactive_titlebar_bg = "rgba(5% 6% 9% 80%)",
+	}
+	config.window_decorations = "RESIZE"
+end
+
+if is_linux then
+	config.window_decorations = "NONE"
+end
 
 config.use_fancy_tab_bar = false
 config.colors = {
-  tab_bar = {
-    background = 'rgba(5% 6% 9% 80%)',
-    inactive_tab = {
-      bg_color = 'rgba(5% 6% 9% 80%)',
-      fg_color = 'rgba(40% 40% 40% 90%)',
-    },
-  },
+	tab_bar = {
+		background = "rgba(5% 6% 9% 80%)",
+		inactive_tab = {
+			bg_color = "rgba(5% 6% 9% 80%)",
+			fg_color = "rgba(40% 40% 40% 90%)",
+		},
+	},
 }
 
-config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.94
 config.macos_window_background_blur = 50
 
 config.font_size = font_size
 
 config.keys = {
-  {
-    key = "Tab",
-    mods = "CTRL",
-    action = wezterm.action.ActivateTabRelative(1),
-  },
-  {
-    key = "w",
-    mods = "CTRL|SHIFT",
-    action = wezterm.action.CloseCurrentPane({ confirm = true }),
-  },
+	{
+		key = "Tab",
+		mods = "CTRL",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		key = "w",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
 }
 
 return config
