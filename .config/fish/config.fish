63c14e3e44d7133b82e00916c263f6d2803b76c1
Yash Shah <yashshah00@outlook.com>
2025-07-29 20:10:23 +0100

    Changed the default Wezterm shell to fish, and added fish configuration files.

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    alias yy="yazi"
+    alias ll="ls -la"
+end
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,6 +2,9 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+-- Spawn a fish shell in login mode
+config.default_prog = { "fish", '-l' }
+
 config.color_scheme = "Blue Scheme"
 
 config.color_schemes = {
